d79937750b674abe20c3c675fa89bf7e5674fcf2
Sahib <sahib1746.cse16@chitkara.edu.in>
2018-06-08 14:11:40 +0530

    Commit 2.0
    Added Authentication and Registration

--- app/src/main/java/tech/rtsproduction/yef/LoginActivity.java
+++ app/src/main/java/tech/rtsproduction/yef/LoginActivity.java
@@ -2,6 +2,7 @@ package tech.rtsproduction.yef;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.ViewPager;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.text.style.CharacterStyle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -18,34 +20,47 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     ImageView logoView;
-    Animation animation,animationLoginView;
+    Animation animation, animationLoginView;
     RelativeLayout loginView;
     Button login;
-    TextInputLayout username,pass;
+    TextInputLayout username, pass;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        login = (Button) findViewById(R.id.LoginButton);
-        username = (TextInputLayout) findViewById(R.id.input_layout_userId);
-        username.setHint(username.getHint().toString().toUpperCase());
+        login = findViewById(R.id.LoginButton);
+        username = findViewById(R.id.input_layout_userId);
+        pass = findViewById(R.id.input_layout_pass);
+        loginView = findViewById(R.id.loginView);
+        logoView = findViewById(R.id.loginLogo);
+        mAuth = FirebaseAuth.getInstance();
 
+        username.setHint(username.getHint().toString().toUpperCase());
         username.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 findViewById(R.id.LoginButton).requestFocusFromTouch();
             }
         });
-        pass = (TextInputLayout) findViewById(R.id.input_layout_pass);
+
         pass.setHint(pass.getHint().toString().toUpperCase());
-        loginView = (RelativeLayout) findViewById(R.id.loginView);
-        logoView = (ImageView) findViewById(R.id.loginLogo);
-        animation = AnimationUtils.loadAnimation(this,R.anim.frombottom);
-        animationLoginView = AnimationUtils.loadAnimation(this,R.anim.splash_anim);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+        animationLoginView = AnimationUtils.loadAnimation(this, R.anim.splash_anim);
         logoView.setAnimation(animation);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -53,19 +68,45 @@ public class LoginActivity extends AppCompatActivity {
             public void run() {
                 loginView.setVisibility(View.VISIBLE);
             }
-        },1500);
+        }, 1500);
+    }
+/*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null){
+            loginSuccessful();
+        }
+    }
+*/
+
+
+    public void onClickGoToSignUp(View view) {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
+        finish();
     }
 
-    public void onClickGoToSignUp(View view)
-    {
-        Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+    public void onClickLogin(View view) {
+        mAuth.signInWithEmailAndPassword(username.getEditText().getText().toString(), pass.getEditText().getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Sign in Successful", Toast.LENGTH_SHORT).show();
+                    loginSuccessful();
+                } else {
+                    Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void loginSuccessful() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
-    public void onClickLogin(View view)
-    {
-        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
+
+
 }
--- app/src/main/java/tech/rtsproduction/yef/SignUpActivity.java
+++ app/src/main/java/tech/rtsproduction/yef/SignUpActivity.java
@@ -1,20 +1,93 @@
 package tech.rtsproduction.yef;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText userName, userEmail, userPass, userConfirmPass;
+    CheckBox termsCheck;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+
+        userName = findViewById(R.id.username);
+        userEmail = findViewById(R.id.email);
+        userPass = findViewById(R.id.pass);
+        userConfirmPass = findViewById(R.id.confirm_pass);
+        termsCheck = findViewById(R.id.termsCheckbox);
+
     }
-    public void onClickSignUp(View view)
-    {
-        Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+
+    public void onClickSignUp(View view) {
+        if (!userName.getText().toString().isEmpty() && !userEmail.getText().toString().isEmpty() && !userPass.getText().toString().isEmpty() && !userConfirmPass.getText().toString().isEmpty()) {
+            if (userPass.getText().toString().matches(userConfirmPass.getText().toString())) {
+                if (termsCheck.isChecked()) {
+                    mAuth.createUserWithEmailAndPassword(userEmail.getText().toString(), userPass.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                UserProfileChangeRequest nameUpdate = new UserProfileChangeRequest.Builder().setDisplayName(userName.getText().toString()).build();
+                                user.updateProfile(nameUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Log.i("Firebase-Private", "Name Updated");
+                                        } else {
+                                            Log.i("Firebase-Private", task.getException().getLocalizedMessage());
+                                        }
+                                    }
+                                });
+                                user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Log.i("Firebase", "Verification Send");
+                                            signupSuccessful();
+                                        } else {
+                                            Log.i("Firebase", task.getException().getLocalizedMessage());
+                                        }
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(SignUpActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(this, "Plese Check Terms and Condition", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Password Don't Match", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Please Fill All Blanks", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void signupSuccessful() {
+        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
