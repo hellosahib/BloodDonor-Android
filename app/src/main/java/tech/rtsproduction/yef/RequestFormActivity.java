96f30eb81cc48e4445f1eecd8ece986b6a3bc072
Sahib <sahib1746.cse16@chitkara.edu.in>
2018-06-11 03:55:42 +0530

    Commit 5.0

--- app/src/main/java/tech/rtsproduction/yef/RequestFormActivity.java
+++ app/src/main/java/tech/rtsproduction/yef/RequestFormActivity.java
@@ -3,21 +3,35 @@ package tech.rtsproduction.yef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RequestFormActivity extends AppCompatActivity {
 
     Button bloodType;
+    EditText dontorName,releation,mobile,location,reason;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("RECORDS");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_form);
         bloodType = findViewById(R.id.bloodSelected);
-
         Bundle parentBundle = getIntent().getExtras();
         String getBlood = parentBundle.getString("bloodType");
-        Toast.makeText(this, getBlood, Toast.LENGTH_SHORT).show();
         bloodType.setText(getBlood);
+
+        dontorName = findViewById(R.id.patientNameEdit);
+        releation = findViewById(R.id.releationEdit);
+        mobile = findViewById(R.id.mobileEdit);
+        location = findViewById(R.id.locationEdit);
+        reason = findViewById(R.id.reasonEdit);
+
+
     }
 }
